3c1005974d9075de8ec87b11836cdcb59b5fb201
VladanStar <vladancupric@gmail.com>
2023-03-06 00:17:01 +0100

    Add

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'temlate-driven-form';
+@ViewChild('f')signupForm!:NgForm;
+  // onSubmit(form:NgForm){
+  //   console.log(form);
+  // }
+
+onSubmit(){
+console.log(this.signupForm)
+}
 }
