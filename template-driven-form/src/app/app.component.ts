38bc537d3507d238480f9fe8c9c24ffbd1eb34c6
VladanStar <vladancupric@gmail.com>
2023-03-06 13:10:55 +0100

    Add

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,21 +6,46 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 @ViewChild('f')signupForm!:NgForm;
 answer:any='';
 answer1:any='';
 answer2:any='';
 answer3:any='';
+answer4:any=''
 model: any = {};
+password:any='';
 formFields=["Tamil Nadu","Kerala"];
 genders =['male','female']
-defaultQuestion = 'pet'
+defaultQuestion = 'pet';
+defaultDrop = 'Advanced'
   // onSubmit(form:NgForm){
   //   console.log(form);
   // }
-  
+  submitted:boolean=false;
+  user={
+    username:"",
+    email:'',
+    password:'',
+    secretQuestion:'',
+    answer:'',
+    dropbox:'',
+    gender:''
+    }
+    selectedOption:string ='';
+    ngOnInit(): void {
+      this.selectedOption='Advanced'
+    }
 onSubmit(){
-console.log(this.signupForm)
+
+this.submitted=true;
+    console.log(this.signupForm);
+this.user.username=this.signupForm.value.userData.username;
+this.user.email=this.signupForm.value.userData.email;
+this.user.password=this.signupForm.value.userData.password;
+this.user.secretQuestion=this.signupForm.value.secret;
+this.user.answer=this.signupForm.value.answer;
+this.user.dropbox=this.signupForm.value.dropbox;
+this.user.gender=this.signupForm.value.gender;
 }
 }
